afa91038fb191bc6aedb9f2e78f89734b4a36aa6
Sakurai Hiroaki <62918873+kaityuu21@users.noreply.github.com>
2021-05-05 18:08:48 +0900

    タスクの追加機能

--- src/index.js
+++ src/index.js
@@ -8,14 +8,31 @@ const onClickAdd = () => {
   // div生成
   const div = document.createElement("div");
   div.className = "list-row";
-
+  // liタグ作成
   const li = document.createElement("li");
   li.innerText = inputText;
   console.log(li);
 
-  div.appendChild(li);
-  console.log(div);
+  // 完了ボタンタグ作成
+  const completedButton = document.createElement("button");
+  completedButton.innerText = "完了";
+  completedButton.addEventListener("click", () => {
+    alert("完了");
+  });
 
+  // 削除ボタンタグ作成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completedButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
 
